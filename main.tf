de21d52484980d9fc25264aa7dfbb1a97d63a048
ahmedshewikh <66251824+ahmedshewikh@users.noreply.github.com>
2025-08-27 14:10:04 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "x" {
   name     = "x"
   location = "East US"  # Change to your preferred region
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "x_vnet" {
   name                = "x-vnet"
   address_space       = ["10.0.0.0/29"]  # 8 IPs total, 5 usable
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.x.location
+  resource_group_name = azurerm_resource_group.x.name
 }
